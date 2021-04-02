14a3b72fb546064ee268bfd6129f9a1d271dbdb0
username <email>
2021-04-02 14:18:25 +0800

    this is the first functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,21 @@
-from selenium import webdriver
+from selenium import webdriver          #(1)
+import unittest
 
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisiotorTest(unittest.TestCase): #(1)
+    def sutUp(self):    #(3)
+        self.browser=webdriver.Firefox() 
+
+    def tearDown(self): #(3)
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a cool new online to-do app. She goes
+        #to check out its homepage
+        self.brower.get('http://localhost:8000')
+        #She notices the page title and header mention to-do lists
+        self.assertIn('TO-DO',self.browser.title)
+        self.fail('Finish the test')
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
