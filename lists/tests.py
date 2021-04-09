c32ab59c41b3f0378e5770d15c67fa4b0b3f61ed
username <email>
2021-04-09 13:47:45 +0800

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,20 @@
 from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page #(2)
+from django.http import HttpRequest
+from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found=resolve('/') #(1)
         self.assertEqual(found.func, home_page) #(1)
+
+    def test_home_page_returns_correct_html(self):
+        request=HttpRequest()
+        response=home_page(request)
+        html=response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.ednswith('</html>'))
 
 # Create your tests here.
