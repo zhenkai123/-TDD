d26db5d7c29b52092cca92819318777e391d5333
username <email>
2021-05-07 13:40:40 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,9 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
@@ -10,11 +11,39 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page) #(1)
 
     def test_home_page_returns_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
+        response=self.client.get('/')
         html=response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+                      
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='The first (ever) list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+        
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
 
 # Create your tests here.
